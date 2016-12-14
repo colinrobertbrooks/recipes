6cab2dce684730acad0c187d178b2575b3d25482
colinrcummings <colinrcummings@gmail.com>
2016-12-13 19:40:13 -0500

    add card formatting, search

--- js/index.js
+++ js/index.js
@@ -10,8 +10,9 @@ Recipes.helpers = {};
 *****/
 Recipes.helpers.processRecipeData = function(data) {
   var result = [];
-  data.forEach(function(d) {
+  data.forEach(function(d, i) {
     result.push({
+      id: i,
       name: d.gsx$name.$t,
       link: d.gsx$link.$t
     });
@@ -25,7 +26,9 @@ Recipes.helpers.processRecipeData = function(data) {
 var App = React.createClass({
   getInitialState: function() {
     return {
-      recipes: null
+      allRecipes: [],
+      searchRecipes: [],
+      searchString: ''
     };
   },
   componentDidMount: function() {
@@ -38,30 +41,79 @@ var App = React.createClass({
       type: 'GET',
       url: url,
       success: (data) => {
+        var recipes = Recipes.helpers.processRecipeData(data.feed.entry);
+        recipes.sort(function(a, b){
+          var nameA = a.name.toLowerCase();
+          var nameB= b.name.toLowerCase();
+          if (nameA < nameB)
+            return -1;
+          if (nameA > nameB)
+            return 1;
+          return 0;
+        });
         this.setState({
-          recipes: Recipes.helpers.processRecipeData(data.feed.entry)
+          allRecipes: recipes,
+          searchRecipes: recipes
         });
       }
     });
   },
+  handleSearch: function(event) {
+    var searchString = event.target.value;
+    var searchRecipes = this.state.allRecipes.filter(function(recipe) {
+      return recipe.name.toLowerCase().search(searchString.toLowerCase()) !== -1;
+    });
+    this.setState({
+      searchRecipes: searchRecipes,
+      searchString: searchString
+    });
+  },
   render: function() {
-    var {recipes} = this.state;
+    var {allRecipes, searchRecipes, searchString} = this.state;
     return (
       <div className="row">
-        <div className="col-xs-12 text-xs-center">
+        <div className="col-xs-12 col-sm-10 offset-sm-1 col-md-12 offset-md-0
+          col-lg-8 offset-lg-2 col-xl-8 offset-xl-2 text-xs-center">
           <h1>Recipes</h1>
+          {allRecipes.length ?
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control text-xs-center"
+                placeholder="search recipes..."
+                style={{
+                  marginTop: '20px'
+                }}
+                value={searchString}
+                onChange={this.handleSearch}
+              />
+              <small
+                className="text-muted"
+                style={{
+                  fontStyle: 'italic'
+                }}
+              >
+                {searchRecipes.length}
+                {searchRecipes.length === 1 ? ' recipe' : ' recipes'}
+              </small>
+            </div>
+          : null}
           <hr/>
-          {recipes ?
-            (recipes.map((recipe, i) => {
-              return (
-                <Recipe
-                  recipe={recipe}
-                  key={i}
-                />
-              );
-            }))
-          :
+          {!allRecipes.length ?
             <p>Loading recipes...</p>
+          :
+            (searchRecipes.length ?
+              (searchRecipes.map((recipe) => {
+                return (
+                  <Recipe
+                    recipe={recipe}
+                    key={recipe.id}
+                  />
+                );
+              }))
+            :
+              <p>No recipes matching <strong>{searchString}</strong>...</p>
+            )
           }
         </div>
       </div>
@@ -76,8 +128,18 @@ var Recipe = React.createClass({
   render: function() {
     var {recipe} = this.props;
     return (
-      <div>
-        <a href={recipe.link} target="_blank">{recipe.name}</a>
+      <div className="card card-block text-xs-center">
+        <h4 className="card-title">
+          {recipe.name}
+        </h4>
+        <a
+          className="btn btn-primary btn-lg"
+          href={recipe.link}
+          target="_blank"
+          title="View recipe"
+        >
+          Recipe
+        </a>
       </div>
     );
   }
