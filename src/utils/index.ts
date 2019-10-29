647c225be08c31b4114b367aa7d5fc9590392176
Colin Cummings <colin_cummings@brown.edu>
2019-10-29 19:44:55 -0400

    Updates

--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as filterRecipes } from './filterRecipes';
-export { default as getRecipeTypeOptions } from './getRecipeTypeOptions';
-export { default as mungeGoogleSheetsEntry } from './mungeGoogleSheetsEntry';
-export { default as sortRecipes } from './sortRecipes';
--- src/utils/sortRecipes.ts
+++
@@ -1,6 +0,0 @@
-import { RecipesType } from '../types';
-
-const sortRecipes = (recipes: RecipesType): RecipesType =>
-  recipes.sort((a, b) => a.name.localeCompare(b.name));
-
-export default sortRecipes;
