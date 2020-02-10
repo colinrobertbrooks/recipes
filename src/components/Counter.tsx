af354202fc7bfcefc9cc560da19c54c0062ef15d
Colin Cummings <colinrcummings@Colins-MacBook-Pro.local>
2020-02-10 09:32:49 -0500

    fix counter pluralization for screen readers; run audit fix

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,14 +4,10 @@ interface CounterProps {
   count: number;
 }
 
-const Counter: React.FC<CounterProps> = ({ count }) => {
-  const plural = count === 1 ? '' : 's';
-
-  return (
-    <small role="alert" aria-live="polite" className="text-muted font-italic">
-      {count} recipe{plural}
-    </small>
-  );
-};
+const Counter: React.FC<CounterProps> = ({ count }) => (
+  <small role="alert" aria-live="polite" className="text-muted font-italic">
+    {count} {count === 1 ? 'recipe' : 'recipes'}
+  </small>
+);
 
 export default Counter;
